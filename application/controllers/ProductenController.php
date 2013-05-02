0ae7fa8e855115de2cc0e49065c335781b3e4773
webmaster <webmaster@L05MAC05.local>
2013-05-02 21:12:39 +0200

    test

--- application/controllers/ProductenController.php
+++ application/controllers/ProductenController.php
@@ -18,7 +18,7 @@ class ProductenController extends Zend_Controller_Action
         //$client = new Zend_Soap_Client('http://192.168.33.95/producten/server?wsdl');
         $client = new Zend_Soap_Client('http://adv1302.mediacampus.be/producten/server?wsdl');
         $client->setSoapVersion(SOAP_1_1); //normaal is het 1.2, voor Zend 1.1
-        $result = $client->addProducts('kris', 'Winsol', 15);
+        $result = $client->addProducts('kris', 'WWW', 15);
         //var_dump($result);
         
     }
